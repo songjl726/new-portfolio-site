208b11d5cf58692c9afb1f51e379e8617a5514fc
LiAn Song <llsong@usc.edu>
2025-07-14 11:14:39 -0600

    updated art portfolio

--- src/data/artworks.js
+++ src/data/artworks.js
@@ -3,15 +3,84 @@ export const artworks = [
     id: 1,
     src: require('../assets/art/full/spider-sketch.png'),     // full-res
     thumb: require('../assets/art/thumb/spider-sketch.jpg'),  // 400-px thumb
-    title: 'Untitled, 2023',
+    title: 'Untitled, 2023 (Procreate)',
     description: 'Sketches I did while listening to lots of shoegaze.'
   },
   {
     id: 2,
+    src: require('../assets/art/full/guy-head.png'),
+    thumb: require('../assets/art/thumb/guy-head.jpg'),
+    title: 'Untitled, 2023 (Procreate)',
+    description: 'Study on facial expression and rendering.'
+  },
+  {
+    id: 3,
+    src: require('../assets/art/full/glasses-girl.png'),
+    thumb: require('../assets/art/thumb/glasses-girl.jpg'),
+    title: 'Untitled, 2023 (Procreate)',
+    description: 'A study on light and different surfaces. Reference image credits user ID1478719616 on Xiaohongshu.'
+  },
+  {
+    id: 4,
+    src: require('../assets/art/full/yves.png'),
+    thumb: require('../assets/art/thumb/yves.jpg'),
+    title: 'Untitled, 2023 (Procreate)',
+    description: 'Reference image credits @mu_gung and @yvesntual on Instagram.'
+  },
+  {
+    id: 5,
     src: require('../assets/art/full/lightstudy.jpeg'),
     thumb: require('../assets/art/thumb/lightstudy.jpg'),
-    title: 'Untitled, 2020',
+    title: 'Untitled, 2020 (oil paint on canvas)',
     description: 'A study of colorful, warm lighting. Reference image credits to BlockBerryCreative.'
   },
-  /* …add more … */
+  {
+    id: 6,
+    src: require('../assets/art/full/Got_Your_Nose.jpeg'),
+    thumb: require('../assets/art/thumb/Got_Your_Nose.jpg'),
+    title: 'Got Your Nose, 2021 (oil paint on canvas)',
+    description: ''
+  },
+  {
+    id: 7,
+    src: require('../assets/art/full/Button.png'),
+    thumb: require('../assets/art/thumb/Button.jpg'),
+    title: 'Button, 2021 (oil paint on canvas)',
+    description: ''
+  },
+  {
+    id: 8,
+    src: require('../assets/art/full/Sunny_Afternoon.jpeg'),
+    thumb: require('../assets/art/thumb/Sunny_Afternoon.jpg'),
+    title: 'Sunny Afternoon, 2021 (oil paint on canvas)',
+    description: ''
+  },
+  {
+    id: 9,
+    src: require('../assets/art/full/ouch.png'),
+    thumb: require('../assets/art/thumb/ouch.jpg'),
+    title: 'Ouch!, 2023 (oil paint on canvas)',
+    description: 'This piece was awarded a Gold Key at the Colorado Scholastic Art Awards and was displayed in the Denver Art Museum.'
+  },
+  {
+    id: 10,
+    src: require('../assets/art/full/The_First_Dance.jpg'),
+    thumb: require('../assets/art/thumb/The_First_Dance.jpg'),
+    title: 'The First Dance, 2022 (oil paint on canvas)',
+    description: 'This piece was awarded a Colorado Scholatic Art Awards Gold Key, National Scholastic Art Awards Gold Medal, and a National Scholastic Art American Visions Award.'
+  },
+  {
+    id: 11,
+    src: require('../assets/art/full/Live_With_It.jpeg'),
+    thumb: require('../assets/art/thumb/Live_With_It.jpg'),
+    title: 'Live With It, 2021 (oil paint on canvas)',
+    description: ''
+  },
+  {
+    id: 12,
+    src: require('../assets/art/full/Have_Your_Cake_and_Eat_it_Too.png'),
+    thumb: require('../assets/art/thumb/Have_Your_Cake_and_Eat_it_Too.jpg'),
+    title: 'Have Your Cake and Eat it Too, 2021 (oil paint on canvas)',
+    description: ''
+  },
 ];
